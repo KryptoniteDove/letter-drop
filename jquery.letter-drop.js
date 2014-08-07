bb95cf378f5412cc8ef9f4a94194ce647911a579
KryptoniteDove <richduncan@gmail.com>
2014-08-07 16:11:26 +0100

    Initial commit

---
+++ jquery.letter-drop.js
@@ -0,0 +1,54 @@
+// Letter Drop version 1.0 by Rich Duncan - //just-rich.io
+
+jQuery.fn.letterDrop = function() {
+  var obj = this;
+  
+  var drop = {
+    arr : obj.text().split( '' ),
+    
+    range : {
+      min : 1,
+      max : 9
+    },
+    
+    styles : function() {
+      var dropDelays = '\n', addCSS;
+      
+       for ( i = this.range.min; i <= this.range.max; i++ ) {
+         dropDelays += '.ld' + i + ' { animation-delay: 1.' + i + 's; }\n';  
+       }
+      
+        addCSS = $( '<style>' + dropDelays + '</style>' );
+        $( 'head' ).append( addCSS );
+    },
+    
+    main : function() {
+      var dp = 0;
+      obj.text( '' );
+      
+      $.each( this.arr, function( index, value ) {
+
+        dp = dp.randomInt( drop.range.min, drop.range.max );
+        
+        if ( value === ' ' )
+          value = '&nbsp';
+        
+          obj.append( '<span class="letterDrop ld' + dp + '">' + value + '</span>' );
+        
+      });
+          
+    }
+  };
+   
+  Number.prototype.randomInt = function ( min, max ) {
+    return Math.floor( Math.random() * ( max - min + 1 ) + min );
+  };
+  
+  
+  // Create styles
+  drop.styles();
+
+  // Initiate
+  drop.main();
+   
+};
